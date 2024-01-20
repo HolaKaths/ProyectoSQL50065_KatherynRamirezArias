fe02f3147a5ff0668793a164c1c1c5783a3d5252
HolaKaths <109301081+HolaKaths@users.noreply.github.com>
2024-01-20 18:53:16 +0100

    insert_datadebmiestilo.sql
    ID seller corrections

--- insert_datadebmiestilo.sql
+++ insert_datadebmiestilo.sql
@@ -1,40 +1,41 @@
 -- Inserción de datos en la tabla Users
 
+-- Inserción de datos de Users
 INSERT INTO Users
-            (username,email,password,created_at,updated_at)
-VALUES      ('Anitanumerouno','anita1@email.com','contrasena1',
-             '2022-01-15 10:30:00',
-             '2022-03-20 15:45:00'),
-            ('Anitanumrodos','anita2@email.com','contrasena2',
-             '2023-02-05 14:20:00',
-             '2023-04-10 09:15:00'),
-            ('Tatiananumerotres','tatiana3@email.com','contrasena3',
-             '2023-03-10 08:45:00',
-             '2023-05-25 12:30:00'),
-            ('Lauranumerocuatro','laura4@email.com','contrasena4',
-             '2023-04-20 17:10:00',
-             '2023-06-30 16:05:00'),
-            ('Vanessanumerocinco','vanessa5@email.com','contrasena5',
-             '2022-05-25 11:55:00',
-             '2022-07-05 14:25:00'),
-            ('Sofianumerouno','sofia1@email.com','contrasena6',
-             '2023-06-10 09:30:00',
-             '2023-08-10 18:40:00'),
-            ('Andreanumerosiete','andrea7@email.com','contrasena7',
-             '2023-07-15 13:15:00',
-             '2023-09-15 10:30:00'),
-            ('Alicenumeroseis','alice6z@email.com','contrasena8',
-             '2023-08-05 16:40:00','2023-09-25 19:15:00'),
-            ('Jorgerojasnueve','jorgerojas9@email.com','contrasena9',
-             '2023-09-01 10:20:00',
-             '2023-09-10 11:50:00'),
-            ('Katherynnumerotrece','katheryn13@email.com','contrasena10',
-             '2024-09-10 14:30:00',
-             '2024-09-15 15:20:00')
-            ('Soyinumerouno','soyiuno@email.com','contrasena10',
-             '2024-12-10 14:30:00',
-             '2024-12-15 15:20:00'); 
-
+      (username,email,password,created_at,updated_at)
+VALUES   ('Anitanumerouno','anita1@email.com','contrasena1',
+       '2022-01-15 10:30:00',
+       '2022-03-20 15:45:00'),
+      ('Anitanumrodos','anita2@email.com','contrasena2',
+       '2023-02-05 14:20:00',
+       '2023-04-10 09:15:00'),
+      ('Tatiananumerotres','tatiana3@email.com','contrasena3',
+       '2023-03-10 08:45:00',
+       '2023-05-25 12:30:00'),
+      ('Lauranumerocuatro','laura4@email.com','contrasena4',
+       '2023-04-20 17:10:00',
+       '2023-06-30 16:05:00'),
+      ('Vanessanumerocinco','vanessa5@email.com','contrasena5',
+       '2022-05-25 11:55:00',
+       '2022-07-05 14:25:00'),
+      ('Sofianumerouno','sofia1@email.com','contrasena6',
+       '2023-06-10 09:30:00',
+       '2023-08-10 18:40:00'),
+      ('Andreanumerosiete','andrea7@email.com','contrasena7',
+       '2023-07-15 13:15:00',
+       '2023-09-15 10:30:00'),
+      ('Alicenumeroseis','alice6z@email.com','contrasena8',
+       '2023-08-05 16:40:00','2023-09-25 19:15:00'),
+      ('Jorgerojasnueve','jorgerojas9@email.com','contrasena9',
+       '2023-09-01 10:20:00',
+       '2023-09-10 11:50:00'),
+      ('Katherynnumerotrece','katheryn13@email.com','contrasena10',
+       '2024-09-10 14:30:00',
+       '2024-09-15 15:20:00'),
+      ('Soyinumerouno','soyiuno@email.com','contrasena10',
+       '2024-12-10 14:30:00',
+       '2024-12-15 15:20:00'), 
+            
 -- Inserción de datos de Productsde Mi Estilo
 
 -- Curso de automaquillaje
@@ -44,7 +45,7 @@ VALUES
 ('Automaquillaje para principiantes',
  'Aprende a maquillarte como una profesional con este curso de automaquillaje para principiantes. En este curso aprenderás las técnicas básicas de maquillaje, desde cómo aplicar la base hasta cómo crear un look de noche.',
  20.00,
- 1,
+ 2,
  NOW(),
  NOW());
 
@@ -66,7 +67,7 @@ VALUES
 ('Peinados para bodas: looks de ensueño',
  'Aprende a crear peinados de boda espectaculares con este curso. En este curso aprenderás a crear diferentes estilos de peinados para bodas, desde moños clásicos hasta trenzas modernas.',
  40.00,
- 1,
+ 2,
  NOW(),
  NOW());
 
@@ -77,7 +78,7 @@ VALUES
 ('Maquillaje para eventos: looks para brillar',
  'Aprende a crear looks de maquillaje perfectos para eventos con este curso. En este curso aprenderás a crear diferentes estilos de maquillaje para eventos, desde maquillaje natural hasta maquillaje de noche.',
  50.00,
- 1,
+ 2,
  NOW(),
  NOW());
 

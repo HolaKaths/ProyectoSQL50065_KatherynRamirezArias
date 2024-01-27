04616b56856c7787d982774e0afd863a29e500e7
HolaKaths <109301081+HolaKaths@users.noreply.github.com>
2024-01-27 17:51:03 +0100

    storedprocedures.sql
    Comentarios sobre la utilidad de los scripts

--- storedprocedures.sql
+++ storedprocedures.sql
@@ -1,25 +1,40 @@
+-- Stored Procedure para obtener el número total de transacciones
+
 CREATE PROCEDURE get_total_transactions(
-IN table_name VARCHAR(255)  = 'transactions',
-IN order_type VARCHAR(255)
+  -- Define el parámetro `table_name`
+  IN table_name VARCHAR(255) = 'transactions',
+  -- Define el parámetro `order_type`
+  IN order_type VARCHAR(255)
 )
 BEGIN
-DECLARE total_transactions INT;
-CREATE TEMPORARY TABLE temp_results (total_transactions INT);
 
-IF order_type = 'ASC' THEN
-INSERT INTO temp_results
-SELECT COUNT(*)
-FROM table_name
-ORDER BY date ASC;
-ELSE
-INSERT INTO temp_results
-SELECT COUNT(*)
-FROM table_name
-ORDER BY date DESC;
-END IF;
+  -- Declara variables
+  DECLARE total_transactions INT;
 
-SELECT * FROM temp_results;
-DROP TABLE temp_results;
+  -- Crea una tabla temporal para almacenar el resultado final
+  CREATE TEMPORARY TABLE temp_results (total_transactions INT);
+
+  -- Ordena los registros según el valor de `order_type` y de forma ascendente
+  IF order_type = 'ASC' THEN
+    INSERT INTO temp_results
+      SELECT COUNT(*)
+      FROM transactions
+      ORDER BY date ASC;
+  ELSE
+    INSERT INTO temp_results
+      SELECT COUNT(*)
+      FROM transactions
+      ORDER BY date DESC;
+  END IF;
+
+  -- Inserta el número total de registros en la tabla temporal, así no ocupo muchos recursos
+  INSERT INTO temp_results
+    SELECT total_transactions;
+
+  -- Devuelve el resultado almacenado en la tabla temporal
+  SELECT * FROM temp_results;
+
+  -- Elimina la tabla temporal para liberar los recursos
+  DROP TABLE temp_results;
+
 END;
-
-

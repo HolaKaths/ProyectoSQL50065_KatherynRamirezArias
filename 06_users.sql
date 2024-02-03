ca9a1b0c71db37e4b35ab9e940986d51f3b87bf5
HolaKaths <109301081+HolaKaths@users.noreply.github.com>
2024-02-03 18:57:06 +0100

    06_users.sql

---
+++ 06_users.sql
@@ -0,0 +1,19 @@
+--  Admini ME: él será el SuperUser con mayor control
+CREATE USER 'AdminMiEstilo'@'localhost' IDENTIFIED BY 'password_pruebaadmin';
+
+-- Recibe: permisos de lectura, inserción, modificación y eliminación en todas las tablas
+GRANT ALL PRIVILEGES ON mi_estilo.* TO 'AdminMiEstilo'@'localhost';
+
+-- Moderador ME
+CREATE USER 'ModMiEstilo'@'localhost' IDENTIFIED BY 'password_pruebamod';
+
+-- Recibe: permisos de lectura, inserción y modificación en todas las tablas. No tendrá permisos de eliminación.
+GRANT SELECT, INSERT, UPDATE ON mi_estilo.* TO 'ModMiEstilo'@'localhost';
+
+
+-- Analista ME
+CREATE USER 'AnalystME'@'localhost' IDENTIFIED BY 'password_pruebaame';
+
+-- Recibe permisos de lectura en todas las tablas. No tendrá permisos de inserción, modificación ni eliminación
+GRANT SELECT ON mi_estilo.* TO 'AnalystME'@'localhost';
+
